8fd658549452fe0afb5a17a2e4ce183bc295d13d
Candeb <candebustos084@gmail.com>
2023-05-08 22:17:52 -0300

    solucion a imagen 404

--- src/data/DataCategories.js
+++ src/data/DataCategories.js
@@ -3,18 +3,18 @@ export const Categories = [
     id: 1,
     title: 'Adidas',
     category: 'Adidas',
-    img: 'src/assets/categories/svg/logoadidas.png',
+    img: 'https://github.com/Candeb/JSX-STORE/blob/main/src/assets/categories/svg/logoadidas.png?raw=true',
   },
   {
     id: 2,
     title: 'Nike',
     category: 'Nike',
-    img: 'src/assets/categories/svg/logonike.png',
+    img: 'https://github.com/Candeb/JSX-STORE/blob/main/src/assets/categories/svg/logonike.png?raw=true',
   },
   {
     id: 3,
     title: 'Balenciaga',
     category: 'Balenciaga',
-    img: 'src/assets/categories/svg/balenciaga.png',
+    img: 'https://github.com/Candeb/JSX-STORE/blob/main/src/assets/categories/svg/balenciaga.png?raw=true',
   },
 ];
