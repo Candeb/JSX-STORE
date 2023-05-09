23c36d65da17522fb6c83ad0a997eb51f985fff1
Candeb <candebustos084@gmail.com>
2023-05-08 21:00:21 -0300

    control de solucion

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -17,7 +17,7 @@ export const ContainerDivsHero = styled.div`
   transition: 0.6s all ease;
 `;
 export const ContainerDivHeroImg = styled(ContainerDivsHero)`
-  background-image: url('src/assets/fondoyellow.png');
+  background-image: url('https://github.com/Candeb/JSX-STORE/blob/main/src/assets/fondoyellow.png?raw=true');
   background-repeat: no-repeat;
   background-size: contain;
   background-position: bottom;
